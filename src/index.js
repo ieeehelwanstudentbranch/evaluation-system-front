d82425259ef33b5c70af218422ac33e365be6b2e
Mohamed Emad <m.emad550@gmail.com>
2019-04-30 14:37:38 +0200

    add redux to application and connect application with it

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers  } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
     
+    
+});
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+
+ReactDOM.render(
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    ,
+    </Provider>,
     document.getElementById('root')
 );
 

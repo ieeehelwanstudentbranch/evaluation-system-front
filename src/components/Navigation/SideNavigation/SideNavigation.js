66ef4070b008a133193cc91ae7e7e27bf192be6a
Mohamed Emad <m.emad550@gmail.com>
2019-04-30 14:33:27 +0200

    add font Icon to project and use it in side navigation

--- src/components/Navigation/SideNavigation/SideNavigation.js
+++ src/components/Navigation/SideNavigation/SideNavigation.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import classes from './SideNavigation.module.scss';
-import NavigationItem from '../NavigationItem/NavigationItem'
+import NavigationItem from '../NavigationItem/NavigationItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faFolder } from '@fortawesome/free-solid-svg-icons';
+import { faGopuram } from '@fortawesome/free-solid-svg-icons';
 
 const sideNavigation = () => (
     <aside>
         <nav>
             <ul>
-                <NavigationItem link="/create-task">Create Task</NavigationItem>
-                <NavigationItem link="/completed-tasks">Completed Tasks</NavigationItem>
-                <NavigationItem link="/pending-tasks">Pending Tasks</NavigationItem>
-                <NavigationItem link="/committees">Committees</NavigationItem>
+                <NavigationItem link="/create-task">
+                    <FontAwesomeIcon icon={faPlus} />
+                    Create Task
+                </NavigationItem>
+                <NavigationItem link="/completed-tasks">
+                    <FontAwesomeIcon icon={faCheck} />
+                    Completed Tasks
+                </NavigationItem>
+                <NavigationItem link="/pending-tasks">
+                    <FontAwesomeIcon icon={faFolder} />
+                    Pending Tasks    
+                </NavigationItem>
+                <NavigationItem link="/committees">
+                    <FontAwesomeIcon icon={faGopuram} />
+                    Committees    
+                </NavigationItem>
             </ul>
         </nav>
     </aside>

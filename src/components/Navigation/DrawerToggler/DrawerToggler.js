8806306057388fba66bb9eefdbab0d49ea8b981c
Mohamed Emad <m.emad550@gmail.com>
2019-05-01 12:37:52 +0200

    UI-Fix: fix displaying items in responsive

--- src/components/Navigation/DrawerToggler/DrawerToggler.js
+++ src/components/Navigation/DrawerToggler/DrawerToggler.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import classes from './DrawerToggler.module.scss';
 
 const drawerToggle = (props)=>(
     
-    <div onClick={props.clicked}>
+    <div className={classes.MenuIcon} onClick={props.clicked}>
         <FontAwesomeIcon icon={props.MobileMenuOpen? faTimes : faBars} clicked={props.drawerToggleClicked}/>
     </div>
 );

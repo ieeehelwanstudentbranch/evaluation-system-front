ae199a49cf9f45648f28d8f476512fdfb90fa58c
mohamednajiub <m.emad550@gmail.com>
2019-06-01 15:59:07 +0200

    add modal component, connect login and registration with auth and modal

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import 'normalize.css';
 import './App.scss';
-
 import Layout from '../hoc/Layout/Layout';
 import { Route } from 'react-router-dom';
 
 import CreateTask from './CreateTask/CreateTask';
 import CompletedTasks from './CompletedTasks/CompletedTasks';
 import PendingTasks from './PendingTasks/PendingTasks';
+import Auth from './Auth/Auth';
 import Login from './Auth/Login/Login';
 import Registration from './Auth/Registration/Registration';
 
@@ -22,8 +22,7 @@ class App extends Component {
           <Route path="/completed-tasks" component={CompletedTasks}/>
           <Route path="/pending-tasks" component={PendingTasks}/>
           <Route path="/committees" render={()=><p>Comittees</p>}/>
-          <Route path="/login" component={Login}/>
-          <Route path="/registration" component={Registration}/>
+          <Route path="/login" component={Auth}/>
         </Layout>
       </div>
     );

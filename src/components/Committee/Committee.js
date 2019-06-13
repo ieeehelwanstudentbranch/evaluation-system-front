63839d4c17b1610790e602589161d69ae98f9b0d
mohamednajiub <m.emad550@gmail.com>
2019-06-13 03:02:30 +0200

    add edit Committee UI

--- src/components/Committee/Committee.js
+++ src/components/Committee/Committee.js
@@ -5,13 +5,14 @@ import { GiSettingsKnobs } from "react-icons/gi";
 const committee = (props) => {
     return(
         <article className={classes.Committee} >
-            <GiSettingsKnobs  className={classes.CommitteeSettings}/>
+            <GiSettingsKnobs  className={classes.CommitteeSettings} onClick={props.editing}/>
             <h3>COMMITTEE NAME</h3>
             {props.mentor ? <p>Mentor: <span>{props.mentor}</span></p> : null}
             {props.director ? <p>Director: <span>{props.director}</span></p> : null}
             {props.hr_od ? <p>HR-Coordinator: <span>{props.hr_od}</span></p> : null}
             {props.numberOfVolunteers ? <p>Number Of Volunteers: <span>{props.numberOfVolunteers}</span></p> : null}
         </article>
+
     )
     
 }
---
+++ src/components/Committee/CommitteeForm/CommitteeForm.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import InputClasses from '../../UI/Input/Input.module.scss';
+import Button from '../../UI/Button/Button';
+class CommitteForm extends Component {
+    state={
+        isAdding: true
+    }
+    
+    render(){
+        const initialValues={
+            name: this.props.name || '',
+            mentor: this.props.mentor || '',
+            director: this.props.director || '',
+            hr_od: this.props.hr_od || ''
+        }
+        const validationSchema = Yup.object().shape({
+            name: Yup.string()
+                .trim()
+                .required('Committee Name is a Required field')
+                .min(3, 'Committee Name is too short it must be at least 3 characters or longer'),
+            mentor: Yup.string()
+                .trim()
+                .required('Please select the Committee mentor'),
+            director: Yup.string()
+                .trim(),
+            hr_od: Yup.string()
+                .trim()
+        });
+        return (
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={this.handleSubmit}
+                render={(FormikProps)=>(
+                    <Form >
+                        {this.props.error? <span>Sorry something went wrong please try again later</span>: null}
+                        {this.props.message? <span>{this.props.message}</span>: null}
+                        <div className={InputClasses.Input}>
+                            <label htmlFor="name" className={InputClasses.Label} >Committee Name <span className="required">*</span></label>
+                            <Field type="name" id="name" name="name" className={InputClasses.InputElement}/>
+                            <ErrorMessage name="name" />
+                        </div>
+                        <div className={InputClasses.Input}>
+                        <label htmlFor="mentor" className={InputClasses.Label} >Committee Mentor <span className="required">*</span></label>
+                            <Field component="select" id="mentor" name="mentor" className={InputClasses.InputElement}>
+                                <option value="">Select Comittee</option>
+                                <option value="1">IT</option>
+                            </Field>
+                            <ErrorMessage name="mentor" />
+                        </div>
+                        <div className={InputClasses.Input}>
+                        <label htmlFor="director" className={InputClasses.Label} >Committee Director <span className="required">*</span></label>
+                            <Field component="select" id="director" name="director" className={InputClasses.InputElement}>
+                                <option value="">Select Comittee</option>
+                                <option value="1">IT</option>
+                            </Field>
+                            <ErrorMessage name="director" />
+                        </div>
+                        <div className={InputClasses.Input}>
+                        <label htmlFor="hr_od" className={InputClasses.Label} >HR-Coordinator <span className="required">*</span></label>
+                            <Field component="select" id="hr_od" name="hr_od" className={InputClasses.InputElement}>
+                                <option value="">Select Comittee</option>
+                                <option value="1">IT</option>
+                            </Field>
+                            <ErrorMessage name="hr_od" />
+                        </div>
+                        <Button type="submit" btnType="Default" disabled={!FormikProps.isValid || FormikProps.isSubmitting}>{this.state.isAdding?'ADD COMMITTEE': 'EDIT COMMITTEE'}</Button>
+                    </Form>
+                )}
+            />
+        )
+    }
+    
+}
+
+export default CommitteForm;

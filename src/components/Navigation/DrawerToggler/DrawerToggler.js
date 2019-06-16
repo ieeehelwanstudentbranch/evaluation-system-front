069c8f42288a6273deed924482be1d6e5887fb27
mohamednajiub <m.emad550@gmail.com>
2019-06-16 21:21:53 +0200

    Replace font awesome icons with react icons in Task component

--- src/components/Navigation/DrawerToggler/DrawerToggler.js
+++ src/components/Navigation/DrawerToggler/DrawerToggler.js
@@ -4,7 +4,6 @@ import {MdMenu, MdClose} from 'react-icons/md';
 
 const drawerToggle = (props)=>(
     <div className={classes.MenuIcon} onClick={props.clicked}>
-        {/* <FontAwesomeIcon icon={props.MobileMenuOpen? faTimes : faBars} clicked={props.drawerToggleClicked}/> */}
         {props.MobileMenuOpen?<MdClose  clicked={props.drawerToggleClicked}/>: <MdMenu  clicked={props.drawerToggleClicked}/>}
     </div>
 );
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import classes from './Task.module.scss';
 import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import {MdCheckCircle} from 'react-icons/md'
 const task = (props) => {
     return(
         <NavLink className={classes.TaskLink} to={props.link} exact={props.exact}>
@@ -13,8 +12,7 @@ const task = (props) => {
                 {props.taskDescription ? <p>{props.taskDetails.taskDescription} </p> : null}
                 {props.sender ? <p>Sender Name: <span>{props.taskDetails.sender}</span></p> : null}
                 {props.reviever ? <p>Volunteer Name: <span>{props.taskDetails.reviever}</span></p>: null}
-                {props.score ? <span className={classes.TaskStatus}>{props.taskDetails.score}</span>: <span className={classes.TaskStatus}><FontAwesomeIcon icon={faCheck}/></span>}
-                {/* <span className={classes.TaskScore}>90</span> */}
+                {props.score ? <span className={classes.TaskStatus}>{props.taskDetails.score}</span>: <span className={classes.TaskStatus}><MdCheckCircle/></span>}
             </article>
         </NavLink>
         

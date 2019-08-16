32823c7da53bc9d4e9357b42c9e8c0f1e4e285f5
mohamednajiub <m.emad550@gmail.com>
2019-08-17 01:55:55 +0200

    UI: fix Login component in moile and tablet views

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import classes from './Toolbar.module.scss';
+import * as classes from './Toolbar.module.scss';
 import Logo from '../../Logo/WhiteLogo';
 import AnonymousNavigation from '../AnonymousNavigation/AnonymousNavigation';
 import AuthenticatedNavigation from '../AuthenticatedNavigation/AuthenticatedNavigation';
@@ -9,7 +9,7 @@ class Toolbar extends Component {
     render(){
         return (
             <header className={[classes.Toolbar, this.props.className].join(' ')}>
-                <Logo height="80%"/>
+                <Logo className={classes.ToolbarLogo}/>
                 <DrawerToggle MobileMenuOpen={this.props.MobileMenuOpen} clicked={this.props.drawerToggleClicked}/>
                 <div className={classes.DesktopOnly}>
                 {this.props.isAuthenticated ?

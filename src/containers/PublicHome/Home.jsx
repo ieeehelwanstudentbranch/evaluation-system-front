7ee62f8beab904152ab05393b5aaf20ed6f56d07
mohamednajiub <m.emad550@gmail.com>
2019-08-17 02:46:36 +0200

    UI: update first section in home component

--- src/containers/PublicHome/Home.jsx
+++ src/containers/PublicHome/Home.jsx
@@ -4,8 +4,14 @@ import * as classes from './Home.module.scss'
 class Home extends Component{
     render(){
         return(
-            <header className={classes.HomeHeader}>
-                <Login className={classes.Login} />
+            <header style={{width: '100%'}}>
+                <div className={`${classes.HomeHeader} container`}>
+                    <div className={classes.Intro}>
+                        <h3>Work Better Together. Schedule Meetings In An Instant.</h3>
+                        <p>An easier way to collaborate with your team or friends. You set up the meeting and set the priority. deicide who to invite and get started</p>
+                    </div>
+                    <Login className={classes.Login} />
+                </div>
             </header>
         )
     }

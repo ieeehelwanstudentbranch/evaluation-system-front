a9a916bdf4cf02a517fc3776099c4a0417c8077f
mohamednajiub <m.emad550@gmail.com>
2019-10-03 14:44:08 +0200

    fix notifications

--- src/components/UI/Notifications/NotificationsContainer/NotificationsContainer.jsx
+++ src/components/UI/Notifications/NotificationsContainer/NotificationsContainer.jsx
@@ -1,64 +1,40 @@
 import React, {Component} from 'react';
 import * as classes from './NotificationsContainer.module.scss';
-import Pusher from 'pusher-js';
 import NotificationItem from '../../../NotificationItem/NotificationItem';
-import axios from '../../../../axios';
+import {connect} from 'react-redux';
 
 class NotificationsContainer extends Component{
-    state={
-        notifications: []
-    }
-    pusher = new Pusher('7b8882c6cc4c6a80206d', {
-        cluster: 'us3',
-        forceTLS: true,
-        encrypted: true,
-    });
-    channel = this.pusher.subscribe("notification");
-    componentDidMount(){
-        axios.get(`/notification`)
-            .then(response=>{
-                this.setState({notifications: [...this.state.notifications, ...response.data]});
-            }).catch(error=>{
-                console.log(error.response)
-            });
-        this.channel.bind('task-created', (data)=>{
-            let Notifications = JSON.parse(JSON.stringify(data));
-            // let totalNotifications = [...this.state.notifications, {...Notifications}]
-            // this.setState({notifications: totalNotifications});
-            this.setState(prevState=>{
-                let newNotifications = prevState.notifications.push(Notifications)
-                return {
-                    notifications: newNotifications
-                }
-            });
-        });
-        this.channel.bind('post-created', (data)=>{
-            let Notifications = JSON.parse(JSON.stringify(data));
-            let totalNotifications = [...this.state.notifications, {...Notifications}]
-            this.setState({notifications: totalNotifications});
-        });
-    }
     
     render(){
-        let sorted = (this.state.notifications&&this.state.notifications.length>0)?
-            this.state.notifications.sort((a,b)=>{
+        let sorted = (this.props.notifications&&this.props.notifications.length>0)?
+            this.props.notifications.sort((a,b)=>{
                 return a.created_at>b.created_at ? -1 : a.created_at<b.created_at ? 1 : 0;
             })
-        :null;
+        :<p>There is no notification that we can display to you</p>;
         return(
             <div className={[classes.NotificationsContainer, this.props.open? classes.Open : classes.Close].join(' ')}>
                 {
                     (sorted&&sorted.length>0)?
-                        sorted.map(notification=>{
+                        sorted.filter(notification=>{
+                            return notification.to === this.props.userID || notification.to === null
+                        }).filter(notification=>{
+                            return notification.from.id !== this.props.userID
+                        }).map(notification=>{
                             return (
                                 <NotificationItem key={notification.id} {...notification}/>
                             )
                         })
-                    :null
+                    :<p>There is no notification that we can display to you</p>
                 }
             </div>
         )
     }
 }
 
-export default NotificationsContainer;
+const mapStateToProps = state => {
+    return {
+        userID: state.user.userData? state.user.userData.id:null
+    }
+}
+
+export default connect (mapStateToProps, null)(NotificationsContainer);
--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -4,11 +4,57 @@ import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideNavigation from '../../components/Navigation/SideNavigation/SideNavigation';
 import { connect } from 'react-redux';
 import NotificationsContainer from '../../components/UI/Notifications/NotificationsContainer/NotificationsContainer';
+import Pusher from 'pusher-js';
+import axios from '../../axios';
 class Layout extends Component {
     state={
         showSideDrawer: false,
         MobileMenuOpen: false,
-        showNotifications: false
+        showNotifications: false,
+        notifications: [],
+        numberOfNotifications: 0
+    }
+    pusher = new Pusher('7b8882c6cc4c6a80206d', {
+        cluster: 'us3',
+        forceTLS: true,
+        encrypted: true,
+    });
+    channel = this.pusher.subscribe("notification");
+    componentDidMount(){
+        axios.get(`/notification`)
+            .then(response=>{
+                this.setState({notifications: [...this.state.notifications, ...response.data]});
+            }).catch(error=>{
+                console.log(error.response)
+            });
+        // this.pusher.connection.bind( 'error', function( err ) {
+        //     console.log(err);
+        // });
+        // this.pusher.connection.bind( 'unavailable', function( ) {
+        //     console.log('unavailable');
+        // });
+        // this.pusher.connection.bind( 'connected', function( ) {
+        //     console.log('connected');
+        // });
+        // this.pusher.connection.bind( 'connecting', function( ) {
+        //     console.log('connecting');
+        // });
+        this.channel.bind('task-created', (data)=>{
+            let Notifications = JSON.parse(JSON.stringify(data));
+            let totalNotifications = [...this.state.notifications, {...Notifications}]
+            this.setState({
+                notifications: totalNotifications,
+                numberOfNotifications: this.state.numberOfNotifications+1
+            });
+        });
+        this.channel.bind('post-created', (data)=>{
+            let Notifications = JSON.parse(JSON.stringify(data));
+            let totalNotifications = [...this.state.notifications, {...Notifications}]
+            this.setState({
+                notifications: totalNotifications,
+                numberOfNotifications: this.state.numberOfNotifications+1
+            });
+        });
     }
     sideDrawerClosedHandler = () =>{
         this.setState({showSideDrawer: false, MobileMenuOpen: false,})
@@ -26,21 +72,22 @@ class Layout extends Component {
         this.setState(prevState => {
             return {
                 ...prevState,
-                showNotifications: !prevState.showNotifications
+                showNotifications: !prevState.showNotifications,
+                numberOfNotifications: 0
             }
         })
     }
     render(){
         return(
             <>
-                <Toolbar isAuthenticated={this.props.isAuthenticated} MobileMenuOpen={this.state.MobileMenuOpen} drawerToggleClicked={this.sideDrawerToggleHandler} notificationsClicked={this.NotificationsHandler}/>
+                <Toolbar isAuthenticated={this.props.isAuthenticated} MobileMenuOpen={this.state.MobileMenuOpen} drawerToggleClicked={this.sideDrawerToggleHandler} notificationsClicked={this.NotificationsHandler} numberOfNotifications={this.state.numberOfNotifications}/>
                 <SideNavigation
                     isAuthenticated={this.props.isAuthenticated}
                     role={this.props.role}
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler}
                 />
-                <NotificationsContainer open={this.state.showNotifications}/>
+                <NotificationsContainer open={this.state.showNotifications} notifications={this.state.notifications}/>
                 <main className={classes.MainApp}>
                     {this.props.children}
                 </main>
